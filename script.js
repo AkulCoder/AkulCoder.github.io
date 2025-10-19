8d35961de5a3a75878595205bfb3108ff4bbdcff
Akul Khugher <106450550+AkulCoder@users.noreply.github.com>
2025-10-19 15:33:35 -0400

    Remove scroll fade-in animation
    Removed fade-in animation functionality on scroll.

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Small fade-in animation when scrolling
 
-// script.js
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".card, .skill-card");
-
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      }
-    });
-  });
-
-  cards.forEach(card => observer.observe(card));
-});

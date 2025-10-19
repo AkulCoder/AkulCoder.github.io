21a05f2b3c84edbddbaa2cd4789a3da80cc5348f
Akul Khugher <106450550+AkulCoder@users.noreply.github.com>
2025-10-19 14:51:07 -0400

    Wrap fade-in animation in DOMContentLoaded event

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Small fade-in animation when scrolling
-const cards = document.querySelectorAll(".card, .skill-card");
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
+
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card, .skill-card");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
   });
+
+  cards.forEach(card => observer.observe(card));
 });
-cards.forEach(card => observer.observe(card));
